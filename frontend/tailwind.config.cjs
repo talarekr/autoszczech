b17162c81bfed74c7d4c867e135d06a5c878c142
talarekr <talarekr@gmail.com>
2025-11-01 00:23:03 +0100

    Update tailwind.config.cjs

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,6 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {}
+  },
   plugins: []
 }
